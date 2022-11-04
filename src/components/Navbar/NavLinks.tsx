dde7d5759ab290911bad6acbafb902e7b20cc35f
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-11-04 00:24:43 -0300

    [Fix] Fix component and add types

--- src/components/Navbar/NavLinks.tsx
+++ src/components/Navbar/NavLinks.tsx
@@ -1,13 +1,14 @@
 import { Link } from "wouter";
-const NavLinks = ({ props: { navRoute, navText } }) => {
+import { FC } from 'react'
+type navData = { props: { navRoute: string, navText: string } }
+const NavLinks: FC<navData> = ({ props: { navRoute, navText } }) => {
   return (
     <Link href={navRoute}>
       <a
-        className={`hover:text-main font-serif font-light text-4xl md:text-2xl color-lightGray ${
-          navText !== "Contact"
-            ? "text-lightGray focus:text-main"
-            : "text-main focus:text-orange-300"
-        } uppercase`}
+        className={`hover:text-main font-serif font-light text-4xl md:text-2xl color-lightGray ${navText !== "Contact"
+          ? "text-lightGray focus:text-main"
+          : "text-main focus:text-orange-300"
+          } uppercase`}
       >
         {navText}_
       </a>
